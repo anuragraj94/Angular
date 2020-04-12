8f148478fc39b9a5938573177b0fb718efdb3273
Anurag <anuragraj94@gmail.com>
2020-04-12 11:18:25 +0530

    pages implemented

--- first/src/app/app-routing.module.ts
+++ first/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {ContactComponent} from './contact/contact.component';
+import {CreateComponent} from './create/create.component';
+import {ListComponent} from './list/list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:  '', pathMatch:  'full',redirectTo:  'home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'create', component: CreateComponent},
+  {path: 'list', component: ListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ first/src/app/list/list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {DataService} from '../data.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  contacts;
+  selectedContact;
+
+  constructor(public dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.contacts = this.dataService.getContacts();
+  }
+  public selectContact(contact){
+    this.selectedContact = contact;
+  }
+
+}
