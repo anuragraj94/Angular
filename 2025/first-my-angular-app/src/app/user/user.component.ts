bfe0ddd5d9f059798c18a0cf0cd0002e10e8b048
Anurag <anuragraj94>
2025-03-03 00:32:23 +0530

    Adding 2025/first-my-angular-app
    New application for learning

---
+++ 2025/first-my-angular-app/src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from "../dummy-users";
+
+
+
+const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length)
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  
+  selectedUser = DUMMY_USERS[randomIndex];
+  
+  get imgagePath() {
+    return 'assets/users/' + this.selectedUser.avatar
+  }
+  
+  onSelectUser(){
+    console.log('Clicked!')
+    const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length)
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
+}
