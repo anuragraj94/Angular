82e0c874e39005a9c2479c944a9722305fe7db55
Anurag <anuragraj94>
2025-03-05 22:28:33 +0530

    Adding models for segregate interfaces

--- 2025/first-my-angular-app/src/app/user/user.component.ts
+++ 2025/first-my-angular-app/src/app/user/user.component.ts
@@ -1,13 +1,10 @@
 import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
+import { type User } from "./user.model";
 
 
 
-interface User {
-  id : string;
-  avatar : string;
-  name : string;
-}
+
 
 
 @Component({
@@ -17,28 +14,20 @@ interface User {
 })
 export class UserComponent {
   
-  /*@Input({required:true}) id!: string;
-  @Input({required:true}) avatar!: string;
-  @Input({required:true}) name!: string;*/
   @Input({required:true}) user!: User;
 
-  /* Another way of input
-  @Input({required:true}) user! : {
-    id : string;
-    avatar : string;
-    name : string;
-  };*/
+  
   @Output() select = new EventEmitter();  
   //select = output<string>();  // new approach
   
   
   public get imgagePath() : string {
-  //  return 'assets/users/' + this.avatar 
     return 'assets/users/' + this.user.avatar 
    }
   
   onSelectUser(){
-    //this.select.emit(this.id);
     this.select.emit(this.user.id);
   }
+
+
 }
