12de2e6c71724273bdf1e756e793acaaff9bcf7d
Anurag <anuragraj94>
2025-03-05 07:46:48 +0530

    change the data binding way

--- 2025/first-my-angular-app/src/app/app.component.ts
+++ 2025/first-my-angular-app/src/app/app.component.ts
@@ -4,20 +4,26 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from "./dummy-users";
+import { TasksComponent } from "./tasks/tasks.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-my-angular-app';
   users = DUMMY_USERS;
+  onSelectedUserId = 'u1';
 
+  get selectedUser(){
+    return this.users.find((x) => x.id === this.onSelectedUserId);
+  }
 
   onSelectUser(id:string){
-console.log("Clicked with " + id);
+this.onSelectedUserId = id;
   }
+
 }
--- 2025/first-my-angular-app/src/app/user/user.component.ts
+++ 2025/first-my-angular-app/src/app/user/user.component.ts
@@ -3,6 +3,12 @@ import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 
 
+interface User {
+  id : string;
+  avatar : string;
+  name : string;
+}
+
 
 @Component({
   selector: 'app-user',
@@ -11,17 +17,28 @@ import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 })
 export class UserComponent {
   
-  @Input({required:true}) id!: string;
+  /*@Input({required:true}) id!: string;
   @Input({required:true}) avatar!: string;
-  @Input({required:true}) name!: string;
+  @Input({required:true}) name!: string;*/
+  @Input({required:true}) user!: User;
+
+  /* Another way of input
+  @Input({required:true}) user! : {
+    id : string;
+    avatar : string;
+    name : string;
+  };*/
   @Output() select = new EventEmitter();  
   //select = output<string>();  // new approach
   
   
   public get imgagePath() : string {
-    return 'assets/users/' + this.avatar  }
+  //  return 'assets/users/' + this.avatar 
+    return 'assets/users/' + this.user.avatar 
+   }
   
   onSelectUser(){
-    this.select.emit(this.id);
+    //this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
