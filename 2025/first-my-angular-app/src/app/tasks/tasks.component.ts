bd7ae46edd4caba9f83a271640d986211f26a0ca
Anurag <anuragraj94>
2025-03-11 21:42:34 +0530

    getting error for onAddTask()

--- 2025/first-my-angular-app/src/app/tasks/tasks.component.ts
+++ 2025/first-my-angular-app/src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from "./new-task/new-task.component";
+import { NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -52,5 +53,21 @@ onStartAddTask(){
   this.isAddingTask = true;
 }
 
+onCancelAddTask(){
+  this.isAddingTask = false;
+}
+
+onAddTask(taskData : NewTaskData):void{
+  this.tasks.unshift({
+    id : new Date().getDate().toString(),
+    userId: this.userId,
+    title:taskData.title,
+    summary:taskData.summary,
+    dueDate:taskData.date
+  })
+  this.isAddingTask =false;
+  console.log("Reaching onAddTask");
+  
+}
 
 }
